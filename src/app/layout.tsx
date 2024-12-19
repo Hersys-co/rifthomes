599608e10477ba66d0f2df4184e97588cb9338dd
Hersys-co <hersysco@gmail.com>
2024-12-19 17:41:55 +0300

    create Hero section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google'
 import Navbar from '@/components/layout/Navbar'
 import './globals.css'
 
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
